8fbb551f437e77355aca8e483a688f682e9df17c
Hugo Dabien <104176100+ddabien@users.noreply.github.com>
2022-07-25 21:33:47 -0300

    Se agregan componentes header,logo-ap y banner

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoApComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/logo-ap/logo-ap.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoApComponent } from './logo-ap.component';
+
+describe('LogoApComponent', () => {
+  let component: LogoApComponent;
+  let fixture: ComponentFixture<LogoApComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogoApComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LogoApComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
